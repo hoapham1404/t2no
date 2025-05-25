981e07dcfa750ca16816946a93467e3ce602e472
superidol <superidol@idontgiveashit.com>
2025-05-25 13:36:49 +0700

    handle github action

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,8 +1,16 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, provideZoneChangeDetection } from "@angular/core";
+import { provideRouter, withHashLocation } from "@angular/router";
 
-import { routes } from './app.routes';
+import { routes } from "./app.routes";
+import { HashLocationStrategy, LocationStrategy } from "@angular/common";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes, withHashLocation()),
+    {
+      provide: LocationStrategy,
+      useClass: HashLocationStrategy,
+    },
+  ],
 };
